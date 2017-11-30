cde093aa21756eb0e4ab2b6e20526c0283885b17
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-30 17:53:26 +0000

    Update client version

--- lib/runner/credentials_config_file.rb
+++ lib/runner/credentials_config_file.rb
@@ -3,13 +3,18 @@ require 'java-properties'
 
 def read_from_config_file(key)
   properties = read_properties_file
-  properties[key]
+  unescape(properties[key])
 end
 
 # noinspection RubyInstanceMethodNamingConvention
 def read_from_config_file_with_default(key, default_value)
   properties = read_properties_file
-  properties.fetch(key, default_value)
+  unescape(properties.fetch(key, default_value))
+end
+
+def unescape(value)
+  value.gsub! '\=', '='
+  value
 end
 
 def read_properties_file
